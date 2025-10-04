cb442674e2ee3633e52f2c2a4582734b6c577a2a
Akash Trivedi <akash.trivedi@fishtownanalytics.com>
2025-10-04 13:26:54 +0000

    studio commit

--- logs/query_log.sql
+++ logs/query_log.sql
@@ -1,18 +1,8 @@
--- created_at: 2025-10-04T12:55:07.972187+00:00
--- finished_at: 2025-10-04T12:55:08.232960+00:00
+-- created_at: 2025-10-04T13:26:06.723010410+00:00
+-- finished_at: 2025-10-04T13:26:06.864731659+00:00
 -- outcome: success
 -- dialect: snowflake
 -- node_id: not available
--- query_id: 01bf7c27-0909-9617-0075-7d8316501622
--- desc: execute adapter call
-describe table raw.jaffle_shop.orders
-/* {"app":"dbt","dbt_version":"2.0.0","profile_name":"default","target_name":"dev","connection_name":""} */;
--- created_at: 2025-10-04T12:55:08.240782+00:00
--- finished_at: 2025-10-04T12:55:08.460402+00:00
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01bf7c27-0909-9725-0075-7d8316502526
--- desc: execute adapter call
-describe table raw.jaffle_shop.customers
-/* {"app":"dbt","dbt_version":"2.0.0","profile_name":"default","target_name":"dev","connection_name":""} */;
+-- query_id: 01bf7c46-0909-9ca6-0075-7d83165044e6
+-- desc: not provided
+select 1 as id;
