9d8cdd548cfa9a34ced2dfbfebf27e412ed62306
melissa woods <melissa.woods+1760390543@dbtlabsworkshop.com>
2025-10-13 21:45:47 +0000

    sources, config

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,7 @@
+{{ config(
+     materialized='view'
+) }}
+
 with customers as (
 
     select
@@ -5,7 +9,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
 
 ),
 
