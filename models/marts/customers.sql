0352a69cbf43bc779ac8bf339416b5ac9b65c8e8
zlata kalyuzhnaya <zlata.kalyuzhnaya@dbtlabsworkshop.com>
2026-09-15 22:35:45 +0000

    datamarts for staging

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -5,7 +5,8 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+--    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers')}}
 
 ),
 
@@ -17,7 +18,8 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+  --  from raw.jaffle_shop.orders
+      from {{ ref('stg_jaffle_shop__orders')}}
 
 ),
 
